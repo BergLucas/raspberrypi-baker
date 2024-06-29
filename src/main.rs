9f2a551a50ebd053ac115f7c2a22144344c7d460
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-06-29 19:01:20 +0200

    Add some commands

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(about = "Build an image from a Bakerfile")]
+    Build {
+        path: String,
+
+        #[arg(short, long)]
+        file: Option<String>,
+
+        #[arg(short, long)]
+        output: Option<String>,
+
+        #[arg(short, long)]
+        tag: Option<String>,
+    },
+    #[command(about = "List images")]
+    Images { image: String },
+    #[command(about = "Remove an image")]
+    Rmi { image: String },
+    #[command(about = "Burn an image to a device")]
+    Burn { device_file: String, image: String },
+}
+
+fn main() {
+    let args = Cli::parse();
+}
